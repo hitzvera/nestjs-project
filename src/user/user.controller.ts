113b99e50b064aa269617f5cf11e5c2ad60d01ca
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-06-08 21:20:09 +0700

    feature(pipe): add uppercase pipe

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query, UsePipes } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UppercasePipe } from 'src/pipes/uppercase/uppercase.pipe';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new UppercasePipe())
   @Get()
-  getUsers() {
-    return this.userService.getUser();
+  getUsers(@Query('name') name: string) {
+    return name;
   }
 }
