cb8920d8d3f80688489790e20c556a6617288350
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2024-06-09 10:56:16 +0700

    feature(interceptors): add response mapping interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './commons/filters/http-exception/http-exception.filter';
+import { ResponseInterceptor } from './commons/interceptors/response/response.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
 bootstrap();
